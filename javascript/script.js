4270754aa333d344e79dd4da1e51489fc5f4ebef
Moses Nandi <monalidor@gmail.com>
2019-03-21 21:59:50 -0400

    Mobile Navigatione

--- javascript/script.js
+++ javascript/script.js
@@ -50,10 +50,27 @@ $('.js--wp-3').waypoint(function(direction) {
     $('.js--wp-3').addClass('animated pulse');
 }, {
     offset: '50%'
-})
+});
 
-  
-  
+/* Animation on scroll */
+
+$('.js--nav-icon').click(function(){
+
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i')
+
+    nav.slideToggle(200);
+
+    if(icon.hasClass('ion-navicon-round')) {
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round');
+    }else {
+        icon.addClass('ion-navicon-round');
+        icon.removeClass('ion-close-round');
+    }
+
+});
+
 
 
 });
